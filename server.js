850ce061b6a51b00508ddd0f85ad76af77a94f17
rocchettomarco <marco@v-research.it>
2022-10-02 23:22:19 +0200

    add external api

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const { join } = require("path");
+const { auth } = require("express-oauth2-jwt-bearer");
+const authConfig = require("./auth_config.json");
+
 const app = express();
+
 
 // Serve static assets from the /public folder
 app.use(express.static(join(__dirname, "public")));
+
+// create the JWT middleware
+const checkJwt = auth({
+  audience: authConfig.audience,
+  issuerBaseURL: `https://${authConfig.domain}`
+});
+
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
 
 // Endpoint to serve the configuration file
 app.get("/auth_config.json", (req, res) => {
@@ -13,6 +29,15 @@ app.get("/auth_config.json", (req, res) => {
 // Serve the index page for all other requests
 app.get("/*", (_, res) => {
   res.sendFile(join(__dirname, "index.html"));
+});
+
+
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
 });
 
 // Listen on port 3000
