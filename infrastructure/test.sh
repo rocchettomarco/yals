1024c11d7ab200e48754af4459dce92d23c852cf
rocchettomarco <marco@v-research.it>
2022-11-21 10:45:53 +0100

    add test script

---
+++ infrastructure/test.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo -e "\nRoot CA - key"
+openssl ecparam -name prime256v1 -genkey -outform pem -out ./cakey.pem
+
+echo -e "\nRoot CA - cert"
+openssl req -new -x509 -days 365 -addext "subjectAltName=DNS:ca.yals.com" -addext "certificatePolicies=2.5.29.32.0" -key ./cakey.pem -out ./cacert.crt -outform pem -subj "/C=IT/ST=Italy/L=Verona/O=YALS Srl/OU=R&D/CN=ca.yals.com/emailAddress=itsec@yals.com"
