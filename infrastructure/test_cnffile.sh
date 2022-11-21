20191fa3a42b2cec5fb763471149cbe89f1cdef2
rocchettomarco <marco@v-research.it>
2022-11-21 12:01:17 +0100

    minor changes in test, add test with cnf file

--- infrastructure/test_cnffile.sh
+++ infrastructure/test_cnffile.sh
@@ -11,6 +11,6 @@ openssl req -new -x509 -days 365 -addext "subjectAltName=DNS:ca.yals.com" -addex
 echo -e "\n*.backend.yals.com - key"
 openssl ecparam -name prime256v1 -genkey -outform pem -out ./backendkey.key
 echo -e "\n*.backend.yals.com - csr"
-openssl req -new -key ./backendkey.key -out ./backend.csr -addext "subjectAltName=DNS:*.backend.yals.com" -addext "certificatePolicies=2.5.29.32.0"
+openssl req -new -key ./backendkey.key -out ./backend.csr -addext "subjectAltName=DNS:*.backend.yals.com" -addext "certificatePolicies=2.5.29.32.0" -config ./openssl.cnf
 echo -e "\n*.backend.yals.com - cert"
-openssl ca -in ./backend.csr -out ./backendcert.crt -key ./cakey.pem -batch
+openssl ca -in ./backend.csr -out ./backendcert.crt -key ./cakey.pem -config ./openssl.cnf -batch
