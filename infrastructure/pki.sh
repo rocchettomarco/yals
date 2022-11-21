82c58a4f91cc8aeffb7dca4b6d2b0a7db4967cf6
rocchettomarco <marco@v-research.it>
2022-11-21 10:39:44 +0100

    minor change

--- infrastructure/pki.sh
+++ infrastructure/pki.sh
@@ -15,7 +15,7 @@ echo -e "\nRoot CA - key"
 openssl ecparam -name prime256v1 -genkey -outform pem -out PKI/private/cakey.pem
 echo -e "\nRoot CA - cert"
 #OID identifier: https://oidref.com/2.5.29.32
-openssl req -new -x509 -days 365 -addext "subjectAltName=DNS:ca.yals.com" -addext "certificatePolicies=2.5.29.32.0" -key PKI/private/cakey.pem -out ./cacert.crt -outform pem -subj "/C=IT/ST=Italy/L=Verona/O=YALS Srl/OU=R&D/CN=ca.yals.com/emailAddress=itsec@yals.com"
+openssl req -new -x509 -days 365 -addext "subjectAltName=DNS:ca.yals.com" -addext "certificatePolicies=2.5.29.32.0" -key PKI/private/cakey.pem -out PKI/public/cacert.crt -outform pem -subj "/C=IT/ST=Italy/L=Verona/O=YALS Srl/OU=R&D/CN=ca.yals.com/emailAddress=itsec@yals.com"
 
 #BACKEND
 echo -e "\n*.backend.yals.com - key"
