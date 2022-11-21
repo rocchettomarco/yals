d2eac29267e9551ab1368e923fa40379a9c569f0
rocchettomarco <marco@v-research.it>
2022-11-21 11:26:54 +0100

    remove subj

--- infrastructure/test.sh
+++ infrastructure/test.sh
@@ -4,4 +4,5 @@ echo -e "\nRoot CA - key"
 openssl ecparam -name prime256v1 -genkey -outform pem -out ./cakey.pem
 
 echo -e "\nRoot CA - cert"
-openssl req -new -x509 -days 365 -addext "subjectAltName=DNS:ca.yals.com" -addext "certificatePolicies=2.5.29.32.0" -key ./cakey.pem -out ./cacert.crt -outform pem -subj "/C=IT/ST=Italy/L=Verona/O=YALS Srl/OU=R&D/CN=ca.yals.com/emailAddress=itsec@yals.com"
+openssl req -new -x509 -days 365 -addext "subjectAltName=DNS:ca.yals.com" -addext "certificatePolicies=2.5.29.32.0" -key ./cakey.pem -out ./cacert.crt -outform pem 
+#-subj "/C=IT/ST=Italy/L=Verona/O=YALS Srl/OU=R&D/CN=ca.yals.com/emailAddress=itsec@yals.com"
